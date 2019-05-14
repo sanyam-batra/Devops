4f34de9a3a4d8a0da154fbc1ac881f2ce71581c5
Sanyam Batra <sanyambatrs13@gmail.com>
2019-05-14 16:45:45 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Welcome to Devops";
+        return "Greetings from Spring Boot!";
     }
     
 }
